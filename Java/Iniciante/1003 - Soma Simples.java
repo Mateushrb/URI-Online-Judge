59d1c07f6b3ecae9bc312455a79b08af8cd282f8
Mateus Henrique <mateushrb@hotmail.com>
2021-08-14 23:44:38 -0300

    Add file via Git

---
+++ Java/Iniciante/1003 - Soma Simples.java
@@ -0,0 +1,19 @@
+package Iniciante;
+
+import java.util.Scanner;
+
+class Main_1003 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        int SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+
+        input.close();
+    }
+    
+}
